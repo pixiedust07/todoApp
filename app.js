aec5c41aa8a8e3c3f5698398c16d2e91eaedd1a7
Sharon Shaji <sharonshaji127001@gmail.com>
2021-06-25 14:28:21 +0530

    Add edit functionality, re-organised makup and stylesheet

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ taskBtn.addEventListener('click', event => {
 });
 
 class UI {
-    // This will add the inputed task along with the delete, edit and status btn
+    // dummy data; for now.
+    static displayTasks() {
+        const tasks = ['Take out trash', 'Do laundry', 'Visit part'];
+
+        tasks.forEach(task => UI.addTask(task));
+    }
+
     static addTask(task) {
         const tbody = document.querySelector('#tasks');
 
         const taskRow = document.createElement('tr');
         taskRow.className += 'task';
         taskRow.innerHTML = `
-            <td><i class="far fa-check-circle"></i></td>
-            <td class="taskItem">${task}</td>
+            <td><i class="far fa-check-circle complete"></i></td>
+            <td>${task}</td>
             <td><a href="#" class="btn" id="editBtn"><i class="fas fa-edit edit"></i></a></td>
             <td><a href="#" class="btn" id="deleteBtn"><i class="fas fa-trash delete"></i></a></td>
         `;
@@ -20,27 +26,35 @@ class UI {
         document.querySelector('#todoInput').value = '';
     }
 
-    static taskEvents(eventTarget) {
-        const targetClassName = eventTarget.classList 
-        if(targetClassName.contains('taskItem')) {
-            targetClassName.toggle('task');
-            eventTarget.previousElementSibling.classList.toggle('statusIcon');
-        } else if(targetClassName.contains('edit')) {
-            // Code to edit the task content
-            // things to do here : 
-            /*
-            on clicking the edit icon, it should change to a save icon until the user clicks it so save the changes.
-            check if .task class is added to the taskContent, if yes then remove it. (This is to remove the stylings applied when the user marks the task as completed)
-            add new attribute contenteditable and set it to true, after editing the text the save icon should save the edited text
-            */
-        } else if(targetClassName.contains('delete')) {
+    static taskEvents(event) {
+        const eventTarget = event.target
+        const targetClassName = event.target.classList 
+        
+        if(targetClassName.contains('complete')) {
+            targetClassName.toggle('statusIcon');
+            eventTarget.parentElement.nextElementSibling.classList.toggle('task');
+        } 
+        
+        else if(targetClassName.contains('edit')) {
+            let textEditableStatus = eventTarget.parentElement.parentElement.previousElementSibling;
+            textEditableStatus.contentEditable=true;
+            eventTarget.className = "fas fa-save save" 
+        } else if (eventTarget.classList.contains('save')) {
+            let textEditableStatus = eventTarget.parentElement.parentElement.previousElementSibling
+            textEditableStatus.contentEditable = false
+            eventTarget.className = "fas fa-edit edit"
+        }
+        
+        else if(targetClassName.contains('delete')) {
             eventTarget.parentElement.parentElement.parentElement.remove();
         }
     }
 }
 
 // Ui events 
+document.addEventListener('DOMContentLoaded', UI.displayTasks);
+
 const tbody = document.querySelector('#tasks');
 tbody.addEventListener('click', event => {
-    UI.taskEvents(event.target);
+    UI.taskEvents(event);
 })
